e4b445e0e06901c4f1da4650d68f37aa278417e4
Roberta Takenaka <roberta.takenaka@scielo.org>
2012-05-31 08:33:38 -0300

    formato para bhl.xml para corrigir url, kw mudanca de diretorio da copia do bhl.xml

--- proc/shs/main_bhl2iahx.sh
+++ proc/shs/main_bhl2iahx.sh
@@ -5,4 +5,6 @@ then
 fi
 mylog=../log/r.`date '+%Y%m%d-%H%M%S'`.log
 sh ./bhl2lilacs.sh download_incr YES YES 2>&1 $mylog
-sh ./call_lilacs2iahx.sh 2>>&1 $mylog
+sh ./call_lilacs2iahx.sh 2>> $mylog
+scp ../db/bhl.xml poseidon:/var/www/search_bhlscielo_org/proc/sci2iahx/scripts/
+scp /var/www/lildbibio_scielo_org/bases/lildbi/dbcertif/lilacs/LILACS.??? poseidon:/var/www/lildbibio_scielo_org/bases/lildbi/dbcertif/lilacs
